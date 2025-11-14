4594562767d3c15c803f271cf5219dac725c4c8d
axromjanyt-commits <axromjanyt@gmail.com>
2025-11-14 16:20:59 +0600

    Update TcP-FrEinD.py

--- TcP-FrEinD.py
+++ TcP-FrEinD.py
@@ -599,7 +599,7 @@ async def process_api_queue():
 async def MaiiiinE():
     global key, iv, region  # <-- অবশ্যই ফাংশনের শুরুতে লিখো
 
-    Uid , Pw = '4152098890','2371E7290309A16F367CA864A627D8827A4B27C18BD87B110C1BD62EBC9602C6'
+    Uid , Pw = '4289072864','C2F6FCD0B6A19FFF775BB15E72987C994B1FEAD13D11EB74674ADED40785C160'
     
     open_id , access_token = await GeNeRaTeAccEss(Uid , Pw)
     if not open_id or not access_token: 
